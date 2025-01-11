27e73ec6791b313aa00bdf024b95222b23af222e
JLarky <jlarky@gmail.com>
2025-01-10 20:49:10 -0700

    prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
 
 export default defineConfig({
-  integrations: [react(), svelte()]
+  integrations: [react(), svelte()],
 });
