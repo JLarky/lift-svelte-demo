c66635c0852bea838ecd6eda5ea1b8b5fe681a2c
JLarky <jlarky@gmail.com>
2025-01-10 21:40:48 -0700

    clean up code a bit

--- src/components/counter.svelte
+++ src/components/counter.svelte
@@ -4,14 +4,16 @@
 
   liftSvelte('my-counter', {
     init(onCleanup) {
-      let count = $state(0);
+      // use incentive to get referenced to existing HTML elements
       const valueEl = findTarget(this, 'value');
       const incrementBtn = findTarget(this, 'increment');
       const decrementBtn = findTarget(this, 'decrement');
 
+      // use rune for our state
+      let count = $state(0);
       // Update display whenever count changes
       $effect(() => {
-        valueEl.textContent = count;
+        valueEl.textContent = `${count}`;
       });
 
       // Create AbortController for cleanup
